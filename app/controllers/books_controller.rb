4bc0d316f40e4f7393ed54ddd3c0eb55a4291a70
Nao1013 <121780890+Nao1013@users.noreply.github.com>
2023-05-13 07:28:29 +0000

    [RSpec]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,14 @@ class BooksController < ApplicationController
   end
   
   def create
+    @books = Book.all
     @book = Book.new(book_params)
-    @book.save
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
   end
 
   def show
@@ -17,6 +22,22 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully edited."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to books_path  # 投稿一覧画面へリダイレクト  
   end
   
   private
